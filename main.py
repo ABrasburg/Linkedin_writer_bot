ecd88ef9eae10cd4a8ce5670034b4a630df78b51
ABrasburg <abrasburg@fi.uba.ar>
2025-05-20 14:45:58 -0300

    test conexion github, obtener repos y commits

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import os
 from fastapi import FastAPI, Request
-from openai import OpenAI
-import telebot
 import httpx
 from dotenv import load_dotenv
+
+from openai import OpenAI
+import telebot
+from github import Github, Auth
 
 load_dotenv()
 
 app = FastAPI()
-client = OpenAI(
-    api_key=os.environ.get("OPEN_AI_KEY"),
-)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
+
+
+chatgpt = OpenAI(
+    api_key=os.environ.get("OPEN_AI_KEY"),
+)
+
+access_token = os.getenv("GITHUB_TOKEN")
+git_auth = Auth.Token(access_token)
+git = Github(auth=git_auth)
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
@@ -21,10 +29,22 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['haiku'])
 def chatgpt_handler(message):
-    response = client.responses.create(
+    response = chatgpt.responses.create(
         model="gpt-4o-mini",
         input = "Write a short haiku"    
     )
     bot.reply_to(message, response.output_text)
+
+user = git.get_user()
+for repo in user.get_repos(affiliation="owner"):
+    if repo.name == "Daily_problems":
+        print(repo.name)
+        commits = repo.get_commits()
+        for commit in commits[:5]:  # Solo los 5 últimos
+            print(f"Mensaje: {commit.commit.message}")
+            print(f"Autor: {commit.commit.author.name}")
+            print(f"Fecha: {commit.commit.author.date}")
+            print("----")
+            print(repo.name)
 
 bot.infinity_polling()
